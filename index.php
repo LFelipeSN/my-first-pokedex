4bc3279f0a5aa61c6ee36cec334739b94ab6680f
hannagabyy <hannarocha000@gmail.com>
2023-01-14 18:59:56 -0300

    Iniciando o front

--- index.php
+++ index.php
@@ -4,25 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/50cab0d7de.js" crossorigin="anonymous"></script>
     <title>POKÉDEX</title>
 </head>
 <body>
-    <h1>Pesquisa Pokémon</h1>
-    <form action = "pesquisa_nome.php" method = "GET"> 
-        <label>
-            <input type = "text" placeholder = "Name do pokémon " name = "name_pokemon"/>
-        </label>
-        <button type = "submit">Procurar</button>
-    </form>
+    <div id="main">
+        <h1>Pokédex</h1>
+        <div id="pesquisa">
+            <form action = "pesquisa_nome.php" method = "GET">
+                    <input class="caixa-pesquisa" type = "text" placeholder = "Name do pokémon " name = "name_pokemon" />
+                    <button class="icone-pesquisa" type = "submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                
+            </form>
+        </div>
+       
+        
+        <div class="card">
+            <img src="poke1.png" alt="pokemon">
+            <p id="nome-pokemon">Bulbasaur</p>
+        </div>
+        
+    </div>
+    
     
 
     <?php        
 
 
-        $con = mysqli_connect('localhost','root','123456');
+        $con = mysqli_connect('localhost','root','admin');
         mysqli_select_db($con, 'pokedex');
         
-        $query = "SELECT * FROM pokémon";
+        $query = "SELECT * FROM pokemon";
     
 	    $result = mysqli_query($con, $query);
 
