d4292cbd8411f53183e5efe823ce5fc0bfd7af79
LFelipeSN <Lufnepomuceno@gmail.com>
2023-01-28 16:28:20 -0300

    new sql script

--- listar_todos.php
+++ listar_todos.php
@@ -25,26 +25,11 @@ if($result):
     <button class="voltar" type = "submit">Voltar</button>
 </form>
 
-<!--
-    <php
-$name_search = $_GET["n"];#nn sei como dar get no valor da pagina(id)
->
 
-image src = ....
 
-<php
- echo ( image_find( $row["pokemon_id"] ) );
-        echo "<br/>";
-        echo $row["pokemon_id"]; 
-        echo " "; 
-        echo $row["name"];
-        
-        ?>
-
-    <form action = <php echo next_pokemon($pokemon_id) ?>#rota pro proximo poke da lista
-        <button type = "submit">proximo pokémon</button>
-    </form>
+<!-- <form action = <php echo next_pokemon($pokemon_id) ?>#rota pro proximo poke da lista
+    <button type = "submit">proximo pokémon</button>
+</form> -->
 
 
 
--->
