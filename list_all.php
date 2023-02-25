a0b0b63c53baab1de4dda1557872eed0e235da2e
hannagabyy <hannarocha000@gmail.com>
2023-02-25 00:03:31 -0300

    arrumando cores de tipo na pokedex

--- list_all.php
+++ list_all.php
@@ -11,7 +11,7 @@
     <title>POKÉDEX</title>
 </head>
 
-<body>
+<body class="">
 <?php
 require("../connect_to_database.php");
 require("functions.php");
@@ -20,7 +20,7 @@ $url = new url;
 $pokemon_id = $_GET["id"];
 ?>
 
-<div class=" justify-itens-center align-itens-center row container">
+<div class="container listar-todos">
     <?php
     list_all($mysqli, $pokemon_id, $url);
     ?>
@@ -31,8 +31,9 @@ $previous_list = ($pokemon_id-1);
 $next_list = ($pokemon_id+1);?>
 
 
+<div class="barra-navegacao">
 <form action = "index.php">
-    <button class="">Voltar</button>
+    <button class="">Página Inicial</button>
 </form>
 
 <?php if( $pokemon_id > 1 ):?>
@@ -42,10 +43,11 @@ $next_list = ($pokemon_id+1);?>
 <?php endif;?>  
 
 <?php if( $pokemon_id < 19 ):?>
-    <div class="">
-        <button class=""><a href = <?php echo $url -> pokemon_list($next_list); ?>>proximos</a></button>
+    <div >
+        <button class=""><a href = <?php echo $url -> pokemon_list($next_list); ?>>Próximo</a></button>
     </div>
 <?php endif;?>  
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
