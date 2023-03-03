4de45bd9ca3065d52e954f9dfc14e1186f4f5021
hannagabyy <hannarocha000@gmail.com>
2023-03-03 20:55:13 -0300

    alinhando e centralizando divs

--- search_pokemon.php
+++ search_pokemon.php
@@ -11,10 +11,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Pesquisa pokémon</title>
 </head>
+
 <body>  
 <div class = "botao-home">
-    <button class="botao-navegacao"><a href="index.php"><i class="fa-solid fa-house"></i></a></button>
-</div>  
+    <a class="botao-navegacao" href="index.php"><i class="fa-solid fa-house"></i></a>
+</div>
     <?php 
     require("functions.php");
     $url = new url;
@@ -22,7 +23,7 @@
     $name_search = $_GET["name_pokemon"];
     ?>           
             
-    <div class="container-fluid col-5"><!--------- centralizar plis-------->
+    <div class="achados-pesquisados"><!--------- centralizar plis-------->
          <?php
         $found_pokemon = $search->search_pokemon($name_search, $url); ?>
     </div>
@@ -30,8 +31,7 @@
     <div class="no_found_pokemon">
         <?php if($found_pokemon == FALSE):
             $image_id = mt_rand(-3, -1);?>    
-            <img src = <?php echo image_find( $image_id )?> alt="pokemon">
-            <p></p>    
+            <img src = <?php echo image_find( $image_id )?> alt="pokemon">  
             <h2> Não conseguimos achar este pokémon!</h2>    
         <?php endif; ?>
     </div>
